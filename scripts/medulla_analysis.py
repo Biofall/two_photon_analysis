aa95d1b43ddfbeeca612e6c4fddc8f09535ced0b
Avery Krieger <krave@stanford.edu>
2022-06-15 15:55:21 -0700

    Cleaning up the file
    cleaning up the file a bit by removing testing and other unnecessary lines

--- scripts/medulla_analysis.py
+++ scripts/medulla_analysis.py
@@ -11,102 +11,100 @@ from visanalysis.analysis import imaging_data, shared_analysis
 from visanalysis.util import plot_tools
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import sem as sem
 import os
 
 # %% LOADING UP THE DATA
 
 experiment_file_directory = '/Users/averykrieger/Documents/local_data_repo/20220527'
-#experiment_file_directory = '/Users/averykrieger/Documents/local_data_repo/example_data/responses/Bruker'
-
 experiment_file_name = '2022-05-27'
-series_number = 16
-opto_condition = True
+series_number = 17
+opto_condition = False
+roi_name = 'distal_medulla'
+
 file_path = os.path.join(experiment_file_directory, experiment_file_name + '.hdf5')
 save_path='/Users/averykrieger/Documents/local_data_repo/figs/'
+saveFig = True
 
 # ImagingDataObject wants a path to an hdf5 file and a series number from that file
 ID = imaging_data.ImagingDataObject(file_path,
                                     series_number,
                                     quiet=False)
 
-# %% ROIS AND RESPONSES
-
 # getRoiResponses() wants a ROI set name, returns roi_data (dict)
-roi_data = ID.getRoiResponses('medulla_rois')
-roi_data.keys()
+roi_data = ID.getRoiResponses(roi_name)
 
-np.unique(ID.getEpochParameters('current_spatial_period'))
+# %% PLOT functions
 
-# %%
-target_sp = np.unique(ID.getEpochParameters('current_spatial_period'))
-target_tf = [0.5, 1, 2, 4]
-fh, ax = plt.subplots((len(target_sp)), len(target_tf), figsize=(12, 12))
-[x.set_ylim([-0.1, +0.9]) for x in ax.ravel()] # list comprehension makes each axis have the same limit for comparisons
-for sp_ind, sp in enumerate(target_sp):
-    for tf_ind, tf in enumerate(target_tf):
-        query = {'current_spatial_period': sp,
-                 'current_temporal_frequency': tf,
-                 'opto_stim': False}
-        no_opto_trials = shared_analysis.filterTrials(roi_data.get('epoch_response'), ID, query=query)
+# For plotting the various conditions in the experiment
+def plotConditionedROIResponses(ID, roi_data, opto_condition, saveFig):
+    """
+    -opto_condition: True or False
+            True used when there is an optogenetic stimulus applied
+            False used when there is no opto, only visual getStimulusTiming
+    -saveFig: True or False
+            True used when a 300dpi pdf of the plot is saved
+            False when no plot saved
+    """
 
-        query = {'current_spatial_period': sp,
-                 'current_temporal_frequency': tf,
-                 'opto_stim': True}
-        opto_trials = shared_analysis.filterTrials(roi_data.get('epoch_response'), ID, query=query)
+    # Find the unique parameter space for this experiment
+    target_sp = np.unique(ID.getEpochParameters('current_spatial_period'))
+    target_tf = np.unique(ID.getEpochParameters('current_temporal_frequency'))
+    tf_number = len(target_tf)
+    roi_number = len(roi_data['roi_response'])
+    color = plt.cm.tab20(np.linspace(0, 1, roi_number))
 
-        ax[sp_ind, tf_ind].plot(roi_data['time_vector'], np.mean(no_opto_trials[0, :, :], axis=0), color='k', alpha=0.8)
-        ax[sp_ind, tf_ind].plot(roi_data['time_vector'], np.mean(opto_trials[0, :, :], axis=0), color='r', alpha=0.8)
-        #pyplot fill_between
-        #struther and riser paper has temporal frequency tuning in on-off layers of medulla
+    # Make the figure axes
+    fh, ax = plt.subplots(len(target_sp), len(target_tf) * roi_number, figsize=(25*roi_number, 25))
+    [x.set_ylim([-0.55, +1.6]) for x in ax.ravel()] # list comprehension makes each axis have the same limit for comparisons
+    for roi_ind in range(roi_number):
+        for sp_ind, sp in enumerate(target_sp):
+            for tf_ind, tf in enumerate(target_tf):
+                # For No Opto Condition (light off)
+                query = {'current_spatial_period': sp,
+                         'current_temporal_frequency': tf,
+                         'opto_stim': False}
+                no_opto_trials = shared_analysis.filterTrials(roi_data.get('epoch_response'), ID, query=query)
+                noOptoMean = np.mean(no_opto_trials[roi_ind, :, :], axis=0)
+                nopt_sem_plus = noOptoMean + sem(no_opto_trials[roi_ind, :, :], axis=0)
+                nopt_sem_minus = noOptoMean - sem(no_opto_trials[roi_ind, :, :], axis=0)
+                ax[sp_ind, tf_ind + (tf_number*roi_ind)].plot(roi_data['time_vector'],
+                    noOptoMean, color='m', alpha=1.0)
+                ax[sp_ind, tf_ind + (tf_number*roi_ind)].fill_between(roi_data['time_vector'],
+                    nopt_sem_plus, nopt_sem_minus, color=color[roi_ind], alpha=0.8)
 
-ID.getRunParameters('pre_time')
-roi_data.keys()
+                if opto_condition == True:
+                    # For Opto Condition (light on)
+                    query = {'current_spatial_period': sp,
+                             'current_temporal_frequency': tf,
+                             'opto_stim': True}
+                    opto_trials = shared_analysis.filterTrials(roi_data.get('epoch_response'), ID, query=query)
+                    optoMean = np.mean(opto_trials[roi_ind, :, :], axis=0)
+                    opto_sem_plus = optoMean + sem(opto_trials[roi_ind, :, :], axis=0)
+                    opto_sem_minus = optoMean - sem(opto_trials[roi_ind, :, :], axis=0)
+                    ax[sp_ind, tf_ind + (tf_number*roi_ind)].plot(roi_data['time_vector'], optoMean, color='k', alpha=0.9)
+                    ax[sp_ind, tf_ind + (tf_number*roi_ind)].fill_between(roi_data['time_vector'],
+                        opto_sem_plus, opto_sem_minus, color='g', alpha=0.6)
 
-roi_data.get('roi_response')[0].shape
-# %%
-fh, ax = plt.subplots(1, 1, figsize=(20, 4))
-ax.plot(roi_data.get('roi_response')[0].T)
+                ax[sp_ind, tf_ind + (tf_number*roi_ind)].set_title(f'ROI:{roi_ind}|SpatPer:{sp}|TempFreq:{tf}', fontsize=14)
+                #struther and riser paper has temporal frequency tuning in on-off layers of medulla
+
+    if saveFig == True:
+        fh.savefig(save_path+str(experiment_file_name)+' | SeriesNumber'+str(series_number)+' | '+str(roi_name) + '.pdf', dpi=300)
+
+# %% CALLING THAT PLOT FUNCTION
+print(noOptoMean.size)
+plotConditionedROIResponses(ID, roi_data, opto_condition, saveFig)
+
+
+# %% Visualing the entire ROI response
+# fh, ax = plt.subplots(1, 1, figsize=(20, 4))
+# ax.plot(roi_data.get('roi_response')[0].T)
 
 # %% Plot trial-average responses by specified parameter name
-ID.getEpochParameters()
-if opto_condition == True:
-    optoQuery = {'opto_stim': True}
-    optoTrials = shared_analysis.filterTrials(roi_data.get('epoch_response'), ID, query=optoQuery)
-    optoTrials.shape
 
-    noOptoQuery = {'opto_stim': False}
-    noOptoTrials = shared_analysis.filterTrials(roi_data.get('epoch_response'), ID, query=noOptoQuery)
-    noOptoTrials.shape
-
-    opto_unique_parameter_values, opto_mean_response, opto_sem_response, opto_trial_response_by_stimulus = ID.getTrialAverages(optoTrials, parameter_key=('current_spatial_period', 'current_temporal_frequency', 'opto_stim'))
-    print(len(opto_unique_parameter_values))
-    opto_uni
-    print(len(optoless_sem_response))
-
-
-    optoless_unique_parameter_values, optoless_mean_response, optoless_sem_response, optoless_trial_response_by_stimulus = imaging_data.ImagingDataObject.getTrialAverages(epoch_response_matrix=noOptoTrials, parameter_key=('current_spatial_period', 'current_temporal_frequency', 'opto_stim'))
-    print(len(opto_unique_parameter_values))
-    opto_unique_parameter_values
-    print(len(optoless_sem_response))
-    optoless_unique_parameter_values
-
-
-else:
-    unique_parameter_values, mean_response, sem_response, trial_response_by_stimulus = ID.getTrialAverages(roi_data.get('epoch_response'), parameter_key=('current_spatial_period', 'current_temporal_frequency', 'opto_stim'))
-    print(len(trial_response_by_stimulus))
-    unique_parameter_values
-
-ID.getTrialAverages
-print(type(unique_parameter_values[1][2]))
-len(unique_parameter_values)
-mean_response.shape
-# roi_data.get('epoch_response').shape
-# roi_data.keys()
-
-
-plotRoiResponsesByCondition(optoless_unique_parameter_values, opto_plot=False)
 plotRoiResponsesByCondition(opto_unique_parameter_values, opto_plot=True)
-
+# %% Deprecated plotting function
 def plotRoiResponsesByCondition(unique_parameter_values, opto_plot=False):
 
     # Find Number of ROIs
@@ -127,19 +125,9 @@ def plotRoiResponsesByCondition(unique_parameter_values, opto_plot=False):
 
     plt.savefig(save_path+str(experiment_file_name)+' | SeriesNumber'+str(series_number)+'.png', dpi=300)
 
-# %% FILTERING FOR OPTO
-
-query = {'opto_stim': True}
-optoOff = shared_analysis.filterTrials(roi_data.get('epoch_resopnse'), ID, query=query)
-unique_parameter_values2, mean_response2, sem_response2, trial_response_by_stimulus2 = ID.getTrialAverages(roi_data.get('epoch_response'), parameter_key=('current_spatial_period', 'current_temporal_frequency', 'opto_stim'))
-
-
-
-
 # %% Some other convenience methods...
 
 # Quickly look at roi responses (averaged across all trials)
 shared_analysis.plotRoiResponses(ID, roi_name='medulla_rois')
 
-# %%
 shared_analysis.plotResponseByCondition(ID, roi_name='medulla_rois', condition='current_temporal_frequency')
