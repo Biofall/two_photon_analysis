ab6c78124a9533d848547d402f100e3844e26b71
Max Turner <maxwellholteturner@gmail.com>
2022-07-28 12:35:31 -0700

    med moco testing

---
+++ scripts/medulla_moco.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --mail-type=END,FAIL
+#SBATCH --job-name=medulla_moco
+#SBATCH --time=12:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --output=/home/users/mhturner/two_photon_analysis/scripts/%x.%j.out
+#SBATCH --open-mode=append
+
+date
+
+directory=$1
+brain_master=$2
+
+echo $directory
+echo $brain_master
+
+# Optional params
+type_of_transform=${3:-"SyN"}
+output_format=${4:-"h5"}
+meanbrain_n_frames=${5:-"100"}
+echo $type_of_transform
+echo $output_format
+echo $meanbrain_n_frames
+
+args="{\"directory\":\"$directory\",\"brain_master\":\"$brain_master\",\"meanbrain_n_frames\":\"$meanbrain_n_frames\","\
+"\"type_of_transform\":\"$type_of_transform\",\"output_format\":\"$output_format\"}"
+
+ml py-ants/0.3.2_py36
+
+python3 -u ./motion_correction.py $args
