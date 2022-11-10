f711d50f22273d8d1e52e680ac8dcf3609bb84ce
Avery Krieger <krave@stanford.edu>
2022-11-10 12:53:09 -0800

    added aff_metric

--- scripts/medulla_moco_anat.sh
+++ scripts/medulla_moco_anat.sh
@@ -20,14 +20,17 @@ echo $brain_master
 # Optional params
 type_of_transform=${4:-"DenseRigid"}
 output_format=${5:-"nii"}
-meanbrain_n_frames=${6:-"500"}
+meanbrain_n_frames=${6:-"100"}
 total_sigma=${7:-"1"}
+aff_metric=${8:-"GC"}
+
 echo $type_of_transform
 echo $output_format
 echo $meanbrain_n_frames
 
 args="{\"directory\":\"$directory\",\"brain_master\":\"$brain_master\",\"brain_mirror\":\"$brain_mirror\",\"meanbrain_n_frames\":\"$meanbrain_n_frames\","\
-"\"total_sigma\":\"$total_sigma\",\"type_of_transform\":\"$type_of_transform\",\"output_format\":\"$output_format\"}"
+"\"total_sigma\":\"$total_sigma\",\"type_of_transform\":\"$type_of_transform\",\"output_format\":\"$output_format\",\"aff_metric\":\"$aff_metric\"}"
+
 
 #ml gcc/6.3.0 py-numpy/1.14.3_py36 py-pandas/0.23.0_py36 viz py-scikit-learn/0.19.1_py36 py-ants/0.3.2_py36
 ml py-ants/0.3.2_py36
