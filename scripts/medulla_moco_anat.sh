b25f0218800f79f05e01f1179bb3a9a41b210a63
Avery Krieger <krave@stanford.edu>
2022-12-08 11:50:34 -0800

    Update medulla_moco_anat.sh
    new node

--- scripts/medulla_moco_anat.sh
+++ scripts/medulla_moco_anat.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --output=/home/users/krave/logs/mocologs/%x.%j.out
 #SBATCH --open-mode=append
-#SBATCH --partition=trc -w sh03-08n13
+#SBATCH --partition=trc -w sh03-08n14
 
 date
 
