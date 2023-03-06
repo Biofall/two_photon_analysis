f63f8bd60537ae377ba4a5ace18a8093544cf425
Max Turner <mhturner@stanford.edu>
2023-03-06 09:50:17 -0800

    moco scratch fn update

--- scripts/moco_scratch.py
+++ scripts/moco_scratch.py
@@ -5,8 +5,8 @@ import nibabel as nib
 
 
 
-filename = 'TSeries-20230216-005_channel_1_aff.nii.gz'
-data_dir = '/Users/mhturner/CurrentData/krieger/20230216'
+filename = 'TSeries-20230223-006_channel_1_aff.nii.gz'
+data_dir = '/Users/mhturner/CurrentData/krieger/20230223'
 
 
 filepath = os.path.join(data_dir, filename)
@@ -19,7 +19,7 @@ filepath = os.path.join(data_dir, filename)
 brain = np.asanyarray(nib.load(filepath).dataobj).astype('uint16')
 
 # save as 16bit
-save_path = os.path.join(data_dir, 'TSeries-20230216-005_channel_1_aff_16.nii')
+save_path = os.path.join(data_dir, 'TSeries-20230223-006_channel_1_aff_16.nii')
 
 nib.save(nib.Nifti1Image(brain, np.eye(4)), save_path)
 
