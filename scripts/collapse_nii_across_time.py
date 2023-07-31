011e65adac4f10a298ef7edef5dddd0169221acc
Avery Krieger <krave@stanford.edu>
2023-07-31 14:19:14 -0700

    just changing inputs

--- scripts/collapse_nii_across_time.py
+++ scripts/collapse_nii_across_time.py
@@ -15,10 +15,10 @@ def collapse_nii(filepath, savepath, n_frames, ouptut_name):
     nib.save(nib.Nifti1Image(np.mean(brain[:, :, :, :n_frames], axis=-1), np.eye(4)), savepath+output_name)
 
 # Set yo variables
-filepath = '/Volumes/ABK2TBData/data_repo/bruker/20230223/TSeries-20230223-anat-004/TSeries-20230223-anat-004_channel_2.nii'
-savepath = '/Volumes/ABK2TBData/data_repo/bruker/20230223/TSeries-20230223-anat-004/'
+filepath = '/Volumes/ABK2TBData/data_repo/bruker/20230519.selected/TSeries-20230519-006_channel_1_aff_16.nii'
+savepath = '/Volumes/ABK2TBData/data_repo/bruker/20230519.selected/'
 n_frames = 'all'
-output_name = 'channel2'
+output_name = 'collapsed'
 
 # run the function
 collapse_nii(filepath, savepath, n_frames, output_name)
